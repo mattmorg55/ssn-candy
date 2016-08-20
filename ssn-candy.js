5fe036a3407ff44ce6b4ebd45c7cd525c25a6633
Matt Morgan <mattmorg55@gmail.com>
2016-08-20 11:39:15 -0400

    Cleaning up code.

--- ssn-candy.js
+++ ssn-candy.js
@@ -1,35 +1,29 @@
 (function (ssn) {
     (function (candy, $, undefined) {
 
-        candy.saySomething = function () {
-            return "hello.";
-        };
-
-        candy.updateInnerHtml = function (elementId, innerHtml) {
-            var fieldNameElement = document.getElementById(elementId);
-            while (fieldNameElement.childNodes.length >= 1) {
-                fieldNameElement.removeChild(fieldNameElement.firstChild);
-            }
-            fieldNameElement.appendChild(fieldNameElement.ownerDocument.createTextNode(innerHtml));
-        }
+        candy.dataValueName = 'ssn-candy-value';
+        candy.cssClassName = 'ssn-candy-mask';
+        candy.isMaskAttributeName = 'ssn-candy-masked';
+        candy.inputmaskPattern = '999-99-9999';
+        candy.inputmaskPlaceholder = ' ';
+        candy.inputmaskJitMasking = true;
+        candy.displayMaskRegex = '';
+        candy.displayMaskReplace = 'XXX-XX';
 
         candy.ssnFocus = function () {
-             //var ssn = $('#ssn');
-                //Retrieve value
-                var value = $(this).data('value');
-                if (value) {
-                    $(this).val(value);
-                }
-                //Mask
-                $(this).inputmask({
-                    "mask": "999-99-9999",
-                    "placeholder": " ",
-                    jitMasking: true
-                });
+            var value = $(this).data(candy.dataValueName);
+            if (value) {
+                $(this).val(value);
+            }
+            //Mask
+            $(this).inputmask({
+                "mask": candy.inputmaskPattern,
+                "placeholder": candy.inputmaskPlaceholder,
+                jitMasking: candy.inputmaskJitMasking
+            });
         }
 
         candy.ssnBlur = function ssnBlur() {
-            //var ssn = $(childSelector);
             var ssnWrapper = $(this).parent();
             if (ssnWrapper) {
                 //Unmask
@@ -37,36 +31,35 @@
                 var newNode = $(this).clone();
                 $(this).inputmask('remove');
                 $(this).remove();
-                //var newNode = $('<input type="text" class="form-control ssn-mask" id="ssn" maxlength="60">');
                 ssnWrapper.append(newNode);
                 //Obscure value
-                newNode.data('value', maskedValue);
-                var obscuredValue = maskedValue.replace(/^\d{3}-\d{2}/, 'XXX-XX');
+                newNode.data(candy.dataValueName, maskedValue);
+                var obscuredValue = maskedValue.replace(/^\d{3}-\d{2}/, candy.displayMaskReplace);
                 newNode.val(obscuredValue);
                 newNode.each(candy.ssnMask);
             }
         }
 
-        candy.ssnValue = function() {
-          var value = $(this).data('value');
-          return value ? value : '';
+        candy.ssnValue = function () {
+            var value = $(this).data(candy.dataValueName);
+            return value ? value : '';
         }
 
-        candy.ssnClear = function() {
-          $(this).data('value', '');
-          $(this).val('');
+        candy.ssnClear = function () {
+            $(this).data(candy.dataValueName, '');
+            $(this).val('');
         }
 
-        candy.ssnMask = function() {
+        candy.ssnMask = function () {
             $(this).focus(candy.ssnFocus);
             $(this).blur(candy.ssnBlur);
-            $(this).attr('ssnmask', true);
+            $(this).attr(candy.isMaskAttributeName, true);
             $(this).init.prototype.ssnValue = candy.ssnValue;
             $(this).init.prototype.ssnClear = candy.ssnClear;
         }
 
-        $(document).ready(function() {
-           $('.ssn-mask').each(candy.ssnMask); 
+        $(document).ready(function () {
+            $('.' + candy.cssClassName).each(candy.ssnMask);
         });
 
     }(window.ssn.candy = window.ssn.candy || {}, jQuery));
